6a3a463248e3bce23e6caa17690f767cf081ef88
Niklas Heckmann <niklasheckmann@MBP-von-Niklas.fritz.box>
2024-05-28 15:54:33 +0200

    Onboarding stage

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -21,6 +21,95 @@ import 'screens/auth/views/welcome_screen_comp/welcome_screen.dart';
 import 'screens/home/blocs/favourite_venue_bloc/favourite_venue_bloc.dart';
 import 'screens/home/blocs/user_bloc/user_bloc.dart';
 
+class MainAppView extends StatelessWidget {
+  final UserRepository userRepository;
+
+  const MainAppView({super.key, required this.userRepository});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Glare Events',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFF8FFA58),
+          onPrimary: Colors.black,
+          surface: Colors.black,
+          onSurface: Color(0xFF8FFA58),
+          background: Colors.black,
+          onBackground: Color(0xFF8FFA58)
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold, color: Color(0xFF8FFA58)),
+          displayMedium: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic, color: Color(0xFF8FFA58)),
+          displaySmall: TextStyle(fontSize: 14.0, fontFamily: 'Hind', color: Color(0xFF8FFA58)),
+        ),
+        buttonTheme: const ButtonThemeData(
+          buttonColor: Color(0xFF13B8A8),
+          textTheme: ButtonTextTheme.primary,
+        )
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: ((context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return MultiBlocProvider(
+              providers: [
+                BlocProvider<SignInBloc>(
+                  create: (context) => SignInBloc(context.read<AuthenticationBloc>().userRepository),
+                ),
+                BlocProvider<SignUpBloc>(
+                  create: (context) => SignUpBloc(context.read<AuthenticationBloc>().userRepository),
+                ),
+                BlocProvider<GetEventBloc>(
+                  create: (context) => GetEventBloc(FirebaseEventRepo())..add(GetEvent()),
+                ),
+                BlocProvider<HomeScreenBloc>(
+                  create: (context) => HomeScreenBloc(),
+                ),
+                BlocProvider<GetVenueBloc>(
+                  create: (context) => GetVenueBloc(FirebaseVenueRepo())..add(GetVenue()),
+                ),
+                BlocProvider<UserBloc>(
+                  create: (context) => UserBloc(userRepository: userRepository),
+                ),
+                BlocProvider<FavouriteVenueBloc>(
+                  create: (context) => FavouriteVenueBloc(context.read<DatabaseRepository>()),
+                ),
+                BlocProvider<OnboardingBloc>(
+                  create: (context) => OnboardingBloc(userRepository: userRepository)
+                    ..add(OnboardingStarted()),
+                ),
+              ],
+              child: BlocBuilder<OnboardingBloc, OnboardingState>(
+                builder: (context, onboardingState) {
+                  if (onboardingState is OnboardingCompletionSuccess) {
+                    return const HomeScreen1();
+                  } else if (onboardingState is OnboardingLoadInProgress) {
+                    return const CircularProgressIndicator();
+                  } else if (onboardingState is OnboardingDataSubmitted) {
+                    return const HomeScreen1();
+                  } else {
+                    return const OnboardingScreen();
+                  }
+                },
+              ),
+            );
+          } else {
+            return const WelcomeScreen();
+          }
+        }),
+      ),
+      routes: {
+        '/welcome': (context) => const WelcomeScreen(),
+        '/home': (context) => const HomeScreen1(),
+        '/signIn': (context) => const SignInScreen(),
+        '/signUp': (context) => const SignUpScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
+      },
+    );
+  }
+}
 
 class MainAppView extends StatelessWidget {
   final UserRepository userRepository;
